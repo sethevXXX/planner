13374b418d8b3dc628ad109706fabde3d1026f9d
sethevXXX <128031046+sethevXXX@users.noreply.github.com>
2023-03-23 23:52:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 window.onload = function(){start()}
 
-
+function hours()
+{
+    let line
+    
+    for (let i = 0; i < 7; i++)
+    {
+        for (let j = 0; j < 24; j++)
+        {
+            line = document.createElement("div")
+            document.getElementsByClassName("plan")[i].appendChild(line)
+            line.className = "hourLines"
+            line.style.top = j * 100 / 24 + 0.5 + "%"
+        }
+    }
+}
 function start()
 {
     
+    hours()
 
 }
