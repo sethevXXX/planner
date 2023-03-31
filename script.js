98bdbde0bfdf5150491b0ac1a82a048a87078f08
sethevXXX <128031046+sethevXXX@users.noreply.github.com>
2023-03-31 03:19:02 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ window.onload = function(){start()}
 
 function hours()
 {
-    let line
-    
+    let time, line
+    for (let i = 0; i < 24; i++)
+    {
+        time = document.createElement("div")
+        document.getElementById("hours").appendChild(time)
+        time.className = "time"
+        if (i < 10) document.getElementsByClassName("time")[i].innerHTML = "0" + i + ":00"
+        else document.getElementsByClassName("time")[i].innerHTML = i + ":00"
+        time.style.top = i * 100 / 24 + "%"
+    }
     for (let i = 0; i < 7; i++)
     {
         for (let j = 0; j < 24; j++)
