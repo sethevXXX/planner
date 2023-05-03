4523586c36de188cba875652b75873b2e815c1f6
sethevXXX <128031046+sethevXXX@users.noreply.github.com>
2023-05-03 13:39:49 +0200

    Add files via upload

---
+++ authentication.php
@@ -0,0 +1,28 @@
+<?php
+
+    require_once "connect.php";
+
+    $connection = new mysqli($host, $dbUser, $dbPassword, $dbName);
+    if($connection -> connect_errno) 
+    {  
+        die("Failed to connect to MySQL: ".$connection -> connect_errno); 
+    }
+    
+    $username = $_POST['username'];
+    $password = $_POST['password']; 
+    
+    $SQL = "SELECT * FROM login WHERE username = '$username' and password = '$password'";  
+    if ($result = $connection -> query($SQL))
+    {
+        echo $result -> num_rows;
+        $row = $result -> fetch_assoc(); 
+        echo $row['username'];
+
+        $result -> free();
+    }  
+        
+    
+    
+
+    $connection -> close();
+?>
---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+    $host = "localhost";
+    $dbUser = "root";
+    $dbPassword = "";
+    $dbName = "planner";
+
+?>
